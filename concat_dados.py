0c908f509c1c532d2c86e8e8610af7a9b55b5768
brendo-hyppolito <brendohyppolito@gmail.com>
2025-09-10 19:22:06 -0300

    reversão do commit anterior

--- concat_dados.py
+++
@@ -1,9 +0,0 @@
-# Solicitar inputs do usuário
-input1 = input("Digite o primeiro texto: ")
-input2 = input("Digite o segundo texto: ")
-
-# Concatenar os inputs
-resultado = input1 + " " + input2
-
-# Exibir o resultado
-print("Resultado da concatenação:", resultado)
